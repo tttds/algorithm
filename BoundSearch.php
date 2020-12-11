4fca150d5a1e493e444548219b4dffa723dae38d
tttds <tadashi_kan@yahoo.co.jp>
2020-12-11 12:33:13 +0900

    Create BoundSearch.php

---
+++ BoundSearch.php
@@ -0,0 +1,109 @@
+<?php
+
+  // $arrayの中で$xより小さい値を持つ最初の位置を返す
+  // 存在しない場合は-1
+  // $arrayは昇順にソートされている前提
+  function seachPosLT(&$a, $x){
+    $ok=-1;
+    $ng=count($a);
+    if($ng === 0) return -1;
+    while($ng-$ok > 1){
+      $m=intdiv($ng+$ok, 2);
+      //echo "m:".$m." ok:".$ok." ng:".$ng." a:".$a[$m]." x:".$x.PHP_EOL;
+      if($a[$m] < $x) $ok = $m;
+      else $ng = $m;
+    }
+    return $ok;
+  }
+
+  // $arrayの中で$x以下の値を持つ最初の位置を返す
+  // 存在しない場合は-1
+  // $arrayは昇順にソートされている前提
+  function seachPosLE(&$a, $x){
+    $ok=-1;
+    $ng=count($a);
+    if($ng === 0) return -1;
+    while($ng-$ok > 1){
+      $m=intdiv($ng+$ok, 2);
+      //echo "m:".$m." ok:".$ok." ng:".$ng." a:".$a[$m]." x:".$x.PHP_EOL;
+      if($a[$m] <= $x) $ok = $m;
+      else $ng = $m;
+    }
+    return $ok;
+  }
+  // $arrayの中で$xより大きい値を持つ最初の位置を返す
+  // 存在しない場合は-1
+  // $arrayは昇順にソートされている前提
+  function seachPosGT(&$a, $x){
+    $ok=count($a);
+    $ng=-1;
+    if($ng === 0) return -1;
+    while($ok-$ng > 1){
+      $m=intdiv($ng+$ok, 2);
+      //echo "m:".$m." ok:".$ok." ng:".$ng." a:".$a[$m]." x:".$x.PHP_EOL;
+      if($a[$m] > $x) $ok = $m;
+      else $ng = $m;
+    }
+    if($ok === count($a)) return -1;
+    return $ok;
+  }
+
+  // $arrayの中で$x以上の値を持つ最初の位置を返す
+  // 存在しない場合は-1
+  // $arrayは昇順にソートされている前提
+  function seachPosGE(&$a, $x){
+    $ok=count($a);
+    $ng=-1;
+    if($ng === 0) return -1;
+    while($ok-$ng > 1){
+      $m=intdiv($ng+$ok, 2);
+      //echo "m:".$m." ok:".$ok." ng:".$ng." a:".$a[$m]." x:".$x.PHP_EOL;
+      if($a[$m] >= $x) $ok = $m;
+      else $ng = $m;
+    }
+    if($ok === count($a)) return -1;
+    return $ok;
+  }
+
+
+  $a = [1, 3, 5, 12, 6, 7, 81, 12];
+  sort($a); // 1, 3, 5, 6, 7, 12, 12, 81
+  
+  $value=7;
+
+  echo "###### searchPosLT Test ######\n";
+  echo seachPosLT($a, 7).PHP_EOL; // (3) 7より小さい値を持つ最初の場所
+  echo seachPosLT($a, 10).PHP_EOL; // (4) 10より小さい値を持つ最初の場所
+  echo seachPosLT($a, 2).PHP_EOL; // (0) 2より小さい値を持つ最初の場所
+  echo seachPosLT($a, 1).PHP_EOL; // (-1) 1より小さい値を持つ最初の場所
+  echo seachPosLT($a, 81).PHP_EOL; // (6) 81より小さい値を持つ最初の場所
+  echo seachPosLT($a, 100).PHP_EOL; // (7) 100より小さい値を持つ最初の場所
+
+
+  echo "###### searchPosLE Test ######\n";
+  echo seachPosLE($a, 7).PHP_EOL; // (4) 7以下の値を持つ最初の場所
+  echo seachPosLE($a, 10).PHP_EOL; // (4) 10以下の値を持つ最初の場所
+  echo seachPosLE($a, 2).PHP_EOL; // (0) 2以下の値を持つ最初の場所
+  echo seachPosLE($a, 1).PHP_EOL; // (0) 1以下の値を持つ最初の場所
+  echo seachPosLE($a, 0).PHP_EOL; // (-1) 0以下の値を持つ最初の場所
+  echo seachPosLE($a, 81).PHP_EOL; // (7) 81以下の値を持つ最初の場所
+  echo seachPosLE($a, 100).PHP_EOL; // (7) 100以下の値を持つ最初の場所
+
+  echo "###### searchPosGT Test ######\n";
+  echo seachPosGT($a, 7).PHP_EOL; // (5) 7より大きい値を持つ最初の場所
+  echo seachPosGT($a, 10).PHP_EOL; // (5) 10より大きい値を持つ最初の場所
+  echo seachPosGT($a, 2).PHP_EOL; // (1)) 2より大きい値を持つ最初の場所
+  echo seachPosGT($a, 1).PHP_EOL; // (1) 1より大きい値を持つ最初の場所
+  echo seachPosGT($a, 0).PHP_EOL; // (0) 0より大きい値を持つ最初の場所
+  echo seachPosGT($a, 81).PHP_EOL; // (-1) 81より大きい値を持つ最初の場所
+  echo seachPosGT($a, 100).PHP_EOL; // (-1) 100より大きい値を持つ最初の場所
+
+  echo "###### searchPosGE Test ######\n";
+  echo seachPosGE($a, 7).PHP_EOL; // (4) 7以上の値を持つ最初の場所
+  echo seachPosGE($a, 10).PHP_EOL; // (5) 10以上の値を持つ最初の場所
+  echo seachPosGE($a, 2).PHP_EOL; // (1)) 2以上の値を持つ最初の場所
+  echo seachPosGE($a, 1).PHP_EOL; // (0) 1以上の値を持つ最初の場所
+  echo seachPosGE($a, 0).PHP_EOL; // (0) 0以上の値を持つ最初の場所
+  echo seachPosGE($a, 81).PHP_EOL; // (7) 81以上の値を持つ最初の場所
+  echo seachPosGE($a, 100).PHP_EOL; // (-1) 100以上の値を持つ最初の場所
+
