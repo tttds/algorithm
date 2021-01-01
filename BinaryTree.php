f58b83a698ca9fb0ada7d725078883389a9cb9a8
tttds <tadashi_kan@yahoo.co.jp>
2021-01-01 14:27:14 +0900

    Update BinaryTree.php

--- BinaryTree.php
+++ BinaryTree.php
@@ -1,5 +1,53 @@
 <?php
  
+$test = new BinaryTree();
+$test->insert(1);
+$test->insert(2);
+$test->insert(3);
+$test->insert(4);
+echo $test->max() === 4 ? "OK" : "NG";
+echo $test->min() === 1 ? "OK" : "NG";
+
+$test->delete(1);
+echo $test->max() === 4 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->delete(4);
+echo $test->max() === 3 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->insert(5);
+echo $test->max() === 5 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->delete(5);
+echo $test->max() === 3 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->insert(1);
+echo $test->max() === 3 ? "OK" : "NG";
+echo $test->min() === 1 ? "OK" : "NG";
+
+$test->delete(1);
+echo $test->max() === 3 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->delete(3);
+echo $test->max() === 2 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->insert(2);
+echo $test->max() === 2 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->delete(2);
+echo $test->max() === 2 ? "OK" : "NG";
+echo $test->min() === 2 ? "OK" : "NG";
+
+$test->delete(2);
+echo $test->max() === -1 ? "OK" : "NG";
+echo $test->min() === -1 ? "OK" : "NG";
+
 class Node {
   public $data;
   public $leftNode;
@@ -18,8 +66,8 @@ class BinaryTree {
   function insert($data){
     $p =& $this->root;
     while($p != null){
-      if($p->data == $data) return false;
-      if($p->data > $data){
+      //if($p->data == $data) return false;
+      if($p->data >= $data){
         $p =& $p->leftNode;
       }else if($p->data < $data){
         $p =& $p->rightNode;
@@ -34,16 +82,19 @@ class BinaryTree {
     while($p != null){
       // データ発見
       if($p->data == $data){
-        // 右の最小のノードを検索
+        // 子が左右両方に存在する場合、右の最小のノードを検索
         if($p->leftNode != null && $p->rightNode != null){
+          $parent =& $p;
           $p2 =& $p->rightNode;
           while($p2->leftNode != null){
+            $parent =& $p2;
             $p2 =& $p2->leftNode;
           }
           // データを更新
           $p->data =& $p2->data;
+          $parent->leftNode = null;
         }        
-        // 左だけ
+        // 子が左だけ存在する場合
         else if($p->leftNode != null){
           //echo "left ".$parent->data ." ".$data.PHP_EOL;
           if($parent != null){
@@ -52,9 +103,12 @@ class BinaryTree {
             }else{
               $parent->rightNode =& $p->leftNode;
             }
+          }else{
+            // 削除対象がrootだった場合
+            $this->root =& $p->leftNode;
           }
         }
-        // 右だけ
+        // 子が右だけ存在する場合
         else if($p->rightNode != null){
           //echo "right ".$parent->data ." ".$data.PHP_EOL;
           if($parent != null){
@@ -63,6 +117,9 @@ class BinaryTree {
             }else{
               $parent->rightNode =& $p->rightNode;
             }
+          }else{
+            // 削除対象がrootだった場合
+            $this->root =& $p->rightNode;
           }
         }
         // 子なし
@@ -74,10 +131,15 @@ class BinaryTree {
             }else {
               $parent->leftNode = null;
             }
+          }else{
+            // 削除対象がrootだった場合
+            $this->root = null;
           }
         }
         return true;
       }
+
+      // データが見つからなかった場合
       $parent =& $p;
       // 左を検索
       if($p->data > $data)
