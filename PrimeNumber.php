4b236acfda066f0a5b2541dccba66889e396924c
tttds <tadashi_kan@yahoo.co.jp>
2021-03-29 12:59:00 +0900

    Update PrimeNumber.php

--- PrimeNumber.php
+++ PrimeNumber.php
@@ -1,5 +1,17 @@
 <?php
-  
+
+var_dump(factorize(10000));
+
+//--------------------------------
+// $n について素因数分解をする
+// 戻り値：連想配列（キーは素数、値は個数）
+// --例1--------------------------
+// $n = 16
+// $return = [2 => 4]
+// --例2--------------------------
+// $n = 24
+// $return = [2 => 3, 3 => 1]
+//--------------------------------
 function factorize($n){
   $res = [];
   for($i=2; $i*$i<=$n; $i++){
@@ -16,51 +28,67 @@ function factorize($n){
   return $res;
 }
 
-  //$pn = new PrimeNumber();
-  //var_dump($pn->primeFactorization(1000000));
+//echo implode(" ", createPrimeNumber(10000));
 
-  class PrimeNumber {
-
-    public $prime = [];
-
-    function createPrimeNumber($n){
-      $sqrt = floor(sqrt($n));
-      $lists = array_fill(2, $n-1, true);
-      for ($i=2; $i<=$sqrt; $i++) {
-        if (isset($lists[$i])) {
-          for ($j=$i*2; $j<=$n; $j+=$i) {
-            unset($lists[$j]);
-          }
-        }
-      }
-      $this->prime = array_keys($lists);
-      return $this->prime;
-    }
-
-    function primeFactorization($value){
-      if(count($this->prime) === 0){
-        $this->createPrimeNumber($value);
-      }
-      $prime_count = count($this->prime);
-      $ret=[];
-      $sqrt=floor(sqrt($value));
-      for($i=0;$i<$prime_count;$i++){
-        // 平方根を超えた場合は残った値が素数
-        if($sqrt < $this->prime[$i]){
-          $ret[$value]=1;
-          return $ret;
-        }
-        // 割り切れる間続ける
-        while($value%$this->prime[$i]==0){
-          if(!isset($ret[$this->prime[$i]])){
-            $ret[$this->prime[$i]]=0;
-          }
-          $ret[$this->prime[$i]]++;
-          $value=intdiv($value, $this->prime[$i]);
-          if($value==1){
-            return $ret;
-          }
-        }
+//--------------------------------
+// $nまでの素数を調べる
+// 戻り値：素数の配列
+// --例1--------------------------
+// $n = 16
+// $return = [2,3,5,7,11,13]
+// --例2--------------------------
+// $n = 24
+// $return = [2,3,5,7,11,13,17,18,23]
+//--------------------------------
+function createPrimeNumber($n){
+  $sqrt = floor(sqrt($n));
+  $lists = array_fill(2, $n-1, true);
+  $prime = [];
+  for ($i=2; $i<=$sqrt; $i++) {
+    if (isset($lists[$i])) {
+      for ($j=$i*2; $j<=$n; $j+=$i) {
+        unset($lists[$j]);
       }
     }
   }
+  $prime = array_keys($lists);
+  return $prime;
+}
+
+$prime = createPrimeNumber(10000);
+var_dump(factorizeUsePrime(10000, $prime));
+
+//--------------------------------
+// $n について素因数分解をする
+// ※事前に素数一覧を作成し、$primeとして渡す
+// 戻り値：連想配列（キーは素数、値は個数）
+// --例1--------------------------
+// $n = 16
+// $return = [2 => 4]
+// --例2--------------------------
+// $n = 24
+// $return = [2 => 3, 3 => 1]
+//--------------------------------
+function factorizeUsePrime($n, $prime){
+  $prime_count = count($prime);
+  $ret=[];
+  $sqrt=floor(sqrt($n));
+  for($i=0;$i<$prime_count;$i++){
+    // 平方根を超えた場合は残った値が素数
+    if($sqrt < $prime[$i]){
+      $ret[$n]=1;
+      return $ret;
+    }
+    // 割り切れる間続ける
+    while($n % $prime[$i]==0){
+      if(!isset($ret[$prime[$i]])){
+        $ret[$prime[$i]]=0;
+      }
+      $ret[$prime[$i]]++;
+      $n=intdiv($n, $prime[$i]);
+      if($n==1){
+        return $ret;
+      }
+    }
+  }
+}
