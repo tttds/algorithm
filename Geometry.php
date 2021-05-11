8f4af3f5b82b7ab6d3a7af96d42698585a12967e
tttds <tadashi_kan@yahoo.co.jp>
2021-05-11 12:29:32 +0900

    Update Geometry.php

--- Geometry.php
+++ Geometry.php
@@ -10,3 +10,14 @@ function isCrossing($ax, $ay, $bx, $by, $cx, $cy, $dx, $dy) {
   return $tc * $td < 0 && $ta * $tb < 0;
   // return $tc * $td <= 0 && $ta * $tb <= 0; // 端点を含む場合
 }
+
+// 3点の作る角度を求める
+// return: (x2,y2)と(x1,y1)を結ぶ線分と、(x2,y2)と(x3,y3)を結ぶ線分の角度（度数）
+function degreeFrom3Points($x1,$y1,$x2,$y2,$x3,$y3){
+    $ax = $x1-$x2;
+    $ay = $y1-$y2;
+    $cx = $x3-$x2;
+    $cy = $y3-$y2;
+    $cos = ($ax*$cx+$ay*$cy) / (sqrt($ax*$ax+$ay*$ay)*sqrt($cx*$cx+$cy*$cy));
+    return rad2deg(acos($cos));
+}
