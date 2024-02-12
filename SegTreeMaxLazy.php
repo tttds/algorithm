7ff1359c0892103d0fa64658ec58fcd5dc4ef48a
tttds <tadashi_kan@yahoo.co.jp>
2024-02-12 23:01:39 +0900

    Update SegTreeMaxLazy.php 非再起に変更して高速化

--- SegTreeMaxLazy.php
+++ SegTreeMaxLazy.php
@@ -1,124 +1,154 @@
 <?php
 
-//-----------------------------------------
-// 区間最大用のLazy Segment Tree
-// 使い方
-// 「書き換える」となっている部分を問題に応じて書き換える
-//-----------------------------------------
-  $N=1;
-  //----------------------------------------------------------
-  // 300010は数列の長さ。間に合わないケースがあるのでその場合は書き換える
-  while ($N < 300010) {
-    $N *= 2;
-  }
-  $tree=array_fill(0, $N*2-1, 0);
-  $lazy=array_fill(0, $N*2-1, 0);
-  //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-  //↓↓書き換える
-  for($i=1;$i<=6;$i=$i+3){
-    update($i, $i+2);
-    update($i+1, $i+1);
-    update($i+2, $i);
-  }
-  echo "---------------------".PHP_EOL;
-  echo query(1, 1).PHP_EOL; // 3 (3)
-  echo query(1, 2).PHP_EOL; // 3 (3, 2)
-  echo query(1, 3).PHP_EOL; // 3 (3, 2, 1)
-  echo query(3, 3).PHP_EOL; // 1 (1)
-  echo query(1, 4).PHP_EOL; // 6 (3, 2, 1, 6)
-  echo query(1, 5).PHP_EOL; // 6 (3, 2, 1, 6, 5)
-  echo query(1, 6).PHP_EOL; // 6 (3, 2, 1, 6, 5, 4)
-  echo query(6, 6).PHP_EOL; // 4 (4)
 
-  echo "---------------------".PHP_EOL;
-  add(1, 3, 3);
-  echo query(1, 1).PHP_EOL; // 6 (6)
-  echo query(1, 2).PHP_EOL; // 6 (6, 5)
-  echo query(1, 3).PHP_EOL; // 6 (6, 5, 4)
-  echo query(3, 3).PHP_EOL; // 4 (4)
-  echo query(1, 4).PHP_EOL; // 6 (6, 5, 4, 6)
-  echo query(1, 5).PHP_EOL; // 6 (6, 5, 4, 6, 5)
-  echo query(1, 6).PHP_EOL; // 6 (6, 5, 4, 6, 5, 4)
-  echo query(6, 6).PHP_EOL; // 4 (4)
+class SegTreeMaxLazy {
 
-  add(3, 5, 4);
-  echo "---------------------".PHP_EOL;
-  echo query(1, 1).PHP_EOL; // 6 (6)
-  echo query(1, 2).PHP_EOL; // 6 (6, 5)
-  echo query(1, 3).PHP_EOL; // 8 (6, 5, 8)
-  echo query(3, 3).PHP_EOL; // 8 (8)
-  echo query(1, 4).PHP_EOL; // 10 (6, 5, 8, 10)
-  echo query(1, 5).PHP_EOL; // 10 (6, 5, 8, 10, 9)
-  echo query(4, 4).PHP_EOL; // 10 (10)
-  echo query(1, 6).PHP_EOL; // 10 (6, 5, 8, 10, 9, 4)
-  echo query(6, 6).PHP_EOL; // 4 (4)
+  private $N = 1;
+  private $tree = null;
+  private $e = 0;
+  private $hierarchy = 0;
 
-  //↑↑書き換える
-  //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+  private $lazy = null;
+  private $no = 0;
 
-  // $i番目の値を$valueで更新する
-  // $iは1から始まる
-  function update($i, $value){
-    global $tree, $N;
-    $i = $N + $i - 2;
-    $tree[$i] = $value;
-    while ($i > 0) {
-      $i = ($i - 1) >> 1;
-      $x = $i << 1;
-      $tree[$i] = max($tree[$x + 1], $tree[$x+ 2]);
-    }
-  }
-  // $start番目から$end番目までの最大値を取得する
-  function query($start, $end){
-    global $N;
-    return sub_query($start-1, $end, 0, 0, $N);
-  }
-
-  function sub_query($a, $b, $k, $l, $r){
-    global $tree;
-    reflect($k,$l,$r);
-    if($b <= $l || $r <= $a) return 0;
-    if($a <= $l && $r <= $b) return $tree[$k];
- 
-    $mid = ($l+$r) >> 1;
-    $x=$k<<1;
-    $lv = sub_query($a, $b, $x+1, $l, $mid);
-    $rv = sub_query($a, $b, $x+2, $mid, $r);
-    return max($lv, $rv);
-  }
-
-  function add($start, $end, $x){
-    global $N;
-    return sub_add($start-1, $end, $x, 0, 0, $N);
-  }
-
-  function sub_add($a, $b, $v, $k, $l, $r){
-    global $tree, $lazy;
-    //reflect($k,$l,$r);
-    if($b <= $l || $r <= $a) return;
-    if($a <= $l && $r <= $b){
-      $lazy[$k] += $v;
-      reflect($k,$l,$r);
-      return;
-    }
-
-    $mid = ($l+$r) >> 1;
-    $x=$k<<1;
-    sub_add($a, $b, $v, $x+1, $l, $mid);
-    sub_add($a, $b, $v, $x+2, $mid, $r);
-    $tree[$k] = max($tree[$x+1], $tree[$x+2]);
-  }
- 
-  function reflect($k,$l,$r){
-    global $tree, $lazy;
-    if($lazy[$k] != 0){
-      $tree[$k] += $lazy[$k];
-      if($r-$l > 1){
-        $x=$k<<1;
-        $lazy[$x+1] += $lazy[$k];
-        $lazy[$x+2] += $lazy[$k];
+  function __construct($N, $e = 0) {
+      $hierarchy = 0;
+      while ($this->N < $N) {
+          $this->N *= 2;
+          ++$hierarchy;
       }
-      $lazy[$k]=0;
-    }
+      $this->tree = array_fill(0, $this->N*2, $e);
+      $this->lazy = array_fill(0, $this->N*2, 0);
+      $this->e = $e;
+      $this->hierarchy = $hierarchy;
   }
 
+  // $x番目の値を$valueで更新する
+  // $xは0から始まる
+  function update($x, $value){
+      $x += $this->N;
+      $tree =& $this->tree;
+      $tree[$x] = $value;
+      while($x){
+          $xnext = $x>>1;
+          //---------------------
+          if($tree[$x] < $tree[$x^1]){
+              $tree[$xnext] = $tree[$x^1];
+          }else{
+              $tree[$xnext] = $tree[$x];
+          }
+          //---------------------
+          $x>>=1;
+      }
+  }
+  // $l番目から$r-1番目までの和を取得する
+  // $lは0から始まる
+  function query($l, $r){
+      $l += $this->N;
+      $r += $this->N;
+      $ids = $this->gindex($l,$r);
+      $this->propagates($ids);
+      $ans = $this->e;
+      $tree =& $this->tree;
+      while($l < $r){
+          //echo ">>".$l." ".$r.PHP_EOL;
+          if($l & 1){
+              //---------------------
+              if($ans < $tree[$l]){
+                  $ans = $tree[$l];
+              }
+              //---------------------
+              ++$l;
+          }
+          if($r & 1){
+              //---------------------
+              if($ans < $tree[$r-1]){
+                  $ans = $tree[$r-1];
+              }
+              //---------------------
+          }
+          $l>>=1;
+          $r>>=1;
+      }
+      return $ans;
+  }
+
+  /**
+   * $l, $rはあらかじめ$this->Nを加算していること
+   */
+  function gindex($l, $r){
+
+      $x = ($l&-$l);
+      $shiftl=0;
+      while($x&1 === 0) $x>>=1; $shiftl++;
+      $lm = $l>>$shiftl;
+      
+      $x = ($r&-$r);
+      $shiftr=0;
+      while($x&1 === 0) $x>>=1; $shiftr++;
+      $rm = $r>>$shiftr;
+
+      $ret = [];
+      while($l < $r){
+          if($l<=$lm) $ret[] = $l;
+          if($r<=$rm) $ret[] = $r;
+          $r>>=1;
+          $l>>=1;
+      }
+      while($l){
+          $ret[] = $l;
+          $l>>=1;
+      }
+      return $ret;
+  }
+
+  function propagates($ids){
+      $ids = array_reverse($ids);
+      $tree =& $this->tree;
+      $lazy =& $this->lazy;
+      foreach($ids as $i){
+          $v = $lazy[$i];
+          if($v === 0) continue;
+          $ii=$i<<1;
+          $lazy[$i] = 0;
+          $lazy[$ii] += $v;
+          $tree[$ii] += $v;
+          $lazy[++$ii] += $v;
+          $tree[$ii] += $v;
+      }
+  }
+  
+  function add($l,$r,$x){
+      $l += $this->N;
+      $r += $this->N;
+      $ids = $this->gindex($l,$r);
+      $tree =& $this->tree;
+      $lazy =& $this->lazy;
+      //echo ">>>>>".$l." ".$r." ".$x.PHP_EOL;
+      //var_dump($ids);
+      while($l < $r){
+          if($l&1){
+              $lazy[$l] += $x;
+              $tree[$l] += $x;
+              ++$l;
+          }
+          if($r&1){
+              $lazy[--$r] += $x;
+              $tree[$r] += $x;
+          }
+          $r >>= 1;
+          $l >>= 1;
+      }
+      foreach($ids as $i){
+          $ii = $i<<1;
+          //---------------------
+          if($tree[$ii] < $tree[$ii+1]){
+              $tree[$i] = $tree[$ii+1];
+          }else{
+              $tree[$i] = $tree[$ii];
+          }         
+          //---------------------
+          $tree[$i] += $lazy[$i];
+      }
+  }
+}
