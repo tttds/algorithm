2a62ca5a45deac102cd46c319d15b6ff9ab1262e
tttds <tadashi_kan@yahoo.co.jp>
2024-03-15 19:41:55 +0900

    Update UnionFind.php

--- UnionFind.php
+++ UnionFind.php
@@ -1,38 +1,35 @@
 <?php
 
-  class UnionFind {
+class UnionFind {
     public $d = [];
     function __construct($n){
-      for($i=1;$i<=$n;$i++){
-        $this->d[$i] = -1;
-      }
+        $d =& $this->d;
+        for($i=1;$i<=$n;++$i){
+            $d[$i] = -1;
+        }
     }
     function unite($x, $y){
-      $rx = $this->root($x);
-      $ry = $this->root($y);
-      if($rx == $ry) return;
-      //echo $rx." ".$ry." ";
-      if($this->size($rx) > $this->size($ry)){
-        $this->d[$rx]+= $this->d[$ry];
-        $this->d[$ry] = $rx;
-        //echo $this->d[$rx].PHP_EOL;
-      }else{
-        $this->d[$ry]+= $this->d[$rx];
-        $this->d[$rx] = $ry;        
-        //echo $this->d[$ry].PHP_EOL;
-      }
+        $rx = $this->root($x);
+        $ry = $this->root($y);
+        if($rx == $ry) return false;
+        $d =& $this->d;
+        if($d[$rx] < $d[$ry]){
+            $d[$rx] += $d[$ry];
+            $d[$ry] = $rx;
+        }else{
+            $d[$ry] += $d[$rx];
+            $d[$rx] = $ry;        
+        }
+        return true;
     }
     function root($x){
-      //echo $x.PHP_EOL;
-      if($this->d[$x] < 0){
-        return $x;
-      }
-      return $this->d[$x] = $this->root($this->d[$x]);
+        if($this->d[$x] < 0) return $x;
+        return $this->d[$x] = $this->root($this->d[$x]);
     }
     function size($x){
-      return $this->d[$this->root($x)]*-1;
+        return $this->d[$this->root($x)]*-1;
     }
     function isSame($x, $y){
-      return $this->root($x) == $this->root($y);        
+        return $this->root($x) == $this->root($y);        
     }
-  }
+}
