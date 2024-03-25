ce8e45274a5e719cb7692fc06f808ca76fbaa48c
tttds <tadashi_kan@yahoo.co.jp>
2024-03-25 16:08:18 +0900

    Update RollingHash.php

--- RollingHash.php
+++ RollingHash.php
@@ -2,18 +2,21 @@
 
 $s = "ababcdabcd";
 $rh = new RollingHash($s);
-echo ($rh->get(0, 2)); // abのハッシュ   = 100009
-echo PHP_EOL;
-echo ($rh->get(2, 4)); // abのハッシュ   = 100009
-echo PHP_EOL;
-echo ($rh->get(1, 3)); // baのハッシュ   = 200015
-echo PHP_EOL;
-echo ($rh->get(0, 4)); // ababのハッシュ = 10598840
-echo PHP_EOL;
-echo ($rh->get(2, 6)); // abcdのハッシュ = 10798856
-echo PHP_EOL;
-echo ($rh->get(6, 10)); // abcdのハッシュ = 10798856
-echo PHP_EOL;
+// abとab
+check($rh->get(0, 2), $rh->get(2, 4));
+// baとbab
+check($rh->get(1, 3), $rh->get(1, 4), false);
+// ababとabcd
+check($rh->get(0, 4), $rh->get(2, 6), false);
+// abcdとabcd
+check($rh->get(2, 6), $rh->get(6, 10));
+
+function check($a, $b, $isSame = true){
+    if($a === $b && $isSame) echo "OK";
+    else if($a !== $b && !$isSame) echo "OK";
+    else echo "NG ". $a." ".$b;
+    echo PHP_EOL;
+}
 
 /**
  * ローリングハッシュ
@@ -23,7 +26,6 @@ class RollingHash {
     private $b;
     private $mod;
     private $s = [];
-    private $t = [];
     private $n;
     private $power = [];
     private $hash = [];
@@ -40,11 +42,6 @@ class RollingHash {
         $this->s =& $s;
         $this->n = $n = strlen($s);
         $alpha = "abcdefghijklmnopqrstuvwxyz";
-        for($i=0;$i<26;$i++) $alphanum[$alpha[$i]]=$i+1;
-        $t =& $this->t;
-        for($i=0;$i<$n;$i++){
-            $t[$i] = $alphanum[$s[$i]];
-        }
         // power
         $power =& $this->power;
         $power[0] = 1;
@@ -53,15 +50,20 @@ class RollingHash {
         }
         // hash
         $hash =& $this->hash;
-        $hash[0] = 0;;
+        $hash[0] = 0;
         for($i=0;$i<$n;++$i){
-            $hash[$i+1] = ($hash[$i] * $b + $t[$i]) % $mod;
+            $hash[$i+1] = ($hash[$i] * $b + ord($s[$i])) % $mod;
         }
     }
 
     /**
-     * s[$l,$r)のハッシュ値を求める
+     * 文字列Sの$l～$r-1のハッシュ値を求める
      * ※ 0-indexed
+     * 例：
+     * S="abcedf"の場合、
+     * get(0,2) = "ab"のハッシュ値
+     * get(3,6) = "ced"のハッシュ値
+     * 
      * @param $l 
      * @param $r 
      * @return Int ハッシュ値
