0e66296ad0ceaeb8ff03ae57bb16bae54b91f8b7
tttds <tadashi_kan@yahoo.co.jp>
2024-04-04 18:21:18 +0900

    Create Binomial.php

---
+++ Binomial.php
@@ -0,0 +1,48 @@
+<?php
+
+$b = new Binomial();
+[$fact, $ifact] = $b->getBinomialInit_mod(5000000, 1000000007);
+echo $b->getBiomial_mod($fact, $ifact, 10, 2, 1000000007);
+
+/**
+ * 二項分布の関連のクラス
+ */
+class Binomial {
+
+    /**
+     * 二項係数の元を求める
+     * 
+     * @param Int $n 二項係数を求める上限（最大5x10^6程度）
+     * @param Int $mod 余り
+     *------------------------------
+     * @return Array [$factorial, $ifactorial]
+     * $factorial・・階乗
+     * $ifactorial・・階乗の逆元 
+     *
+     */
+    public function getBinomialInit_mod($n, $mod){
+        $factorial = [1,1];
+        $ifactorial = [1,1];
+        $inv = [1,1];
+        for($i=2;$i<=$n;$i++){
+            $factorial[$i] = $factorial[$i-1] * $i % $mod;
+            $inv[$i] = $mod - $inv[$mod%$i] * intdiv($mod, $i) % $mod;
+            $ifactorial[$i] = $ifactorial[$i-1] * $inv[$i] % $mod;
+        }
+        return [$factorial, $ifactorial];
+    }
+
+    /**
+     * nCkを求める
+     * @param Array $fact 階乗
+     * @param Array $ifact 階乗の逆元
+     * @param Int $n nCkのn
+     * @param Int $k nCkのk
+     * @param Int $mod 余り
+     */
+    public function getBiomial_mod($fact, $ifact, $n, $k, $mod){
+        if ($n < $k) return 0;
+        if ($n < 0 || $k < 0) return 0;
+        return $fact[$n] * ($ifact[$k] * $ifact[$n - $k] % $mod) % $mod;
+    }
+}
